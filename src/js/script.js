d58b2a483e83ad1cbcae4bd8e126cdc970ee913b
Magdalena Świderska <m.swiderska@outlook.com>
2020-05-25 11:05:04 +0200

    Add initActions

--- src/js/script.js
+++ src/js/script.js
@@ -246,6 +246,7 @@
 
       thisWidget.getElements(element);
       thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
 
       console.log('AmountWidget:', thisWidget);
       console.log('constructor arguments:', element);
@@ -271,6 +272,24 @@
       thisWidget.input.value = thisWidget.value;
     }
 
+    initActions() {
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function () {
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+
+    }
 
   }
 
