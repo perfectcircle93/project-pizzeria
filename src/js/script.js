8e459d264ea4a64af3704db5734297e5fecdd8b1
Magdalena Świderska <m.swiderska@outlook.com>
2020-05-26 23:44:14 +0200

    Add initActions in class Cart hide&show effect

--- src/js/script.js
+++ src/js/script.js
@@ -346,6 +346,45 @@
 
   }
 
+  class Cart {
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      /* START: click event listener to trigger */
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+
+        /* prevent default action for event */
+        event.preventDefault();
+
+        /* toggle class on element of thisCart */
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+
+      /* END: click event listener to trigger */
+      });
+
+    }
+
+  }
 
   const app = {
     initMenu: function(){
@@ -356,11 +395,20 @@
         new Product(productData, thisApp.data.products[productData]);
       }
     },
+
     initData: function(){
       const thisApp = this;
 
       thisApp.data = dataSource;
     },
+
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       //console.log('*** App starting ***');
@@ -371,7 +419,9 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
+
   };
 
   app.init();
